9229bfa408b20bf7e0002af1f711193462fd9f2e
yanaazkha68-cmyk <yanaazkha68@gmail.com>
2026-09-19 09:24:00 +0700

    Create Theme.kt

---
+++ app/src/main/java/com/ternakpro/app/ui/theme/Theme.kt
@@ -0,0 +1,28 @@
+package com.ternakpro.app.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColors = lightColorScheme()
+
+private val DarkColors = darkColorScheme()
+
+@Composable
+fun TernakProTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColors
+    } else {
+        LightColors
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
+}
